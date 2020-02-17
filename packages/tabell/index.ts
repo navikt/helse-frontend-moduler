f4183f229a1526a37f1494d0b4cdfb2a89270785
Stephen Ramthun <stephenramthun@gmail.com>
2020-02-17 15:43:52 +0100

    Ikke pakk med source map

--- packages/tabell/index.ts
+++ packages/tabell/index.ts
@@ -1,7 +1,7 @@
-import Tabell from './Tabell';
+import Tabell, { Body, Header, Footer } from './Tabell';
 import Periodetabell from './periodetabell';
 import { Dag, Dagtype, Kilde } from './periodetabell/types';
 
-export { Periodetabell, Tabell, Dag, Dagtype, Kilde };
+export { Periodetabell, Tabell, Dag, Dagtype, Kilde, Body, Header, Footer };
 
 export default Tabell;
