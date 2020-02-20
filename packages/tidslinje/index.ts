2f8216b621496d1cd1de58cf81033a0e7253fd61
Stephen Ramthun <stephenramthun@gmail.com>
2020-02-20 10:00:42 +0100

    Prøv å bundle riktig

--- packages/tidslinje/index.ts
+++ packages/tidslinje/index.ts
@@ -1,3 +1,13 @@
 import Tidslinje from './components/Tidslinje';
+import {
+    Intervall,
+    Skalapunkt,
+    Inntektstype,
+    Vedtaksperiode,
+    EnkelTidslinje,
+    Skalastørrelse,
+    VedtaksperiodeStatus
+} from './types';
 
+export { Intervall, Skalapunkt, Inntektstype, Vedtaksperiode, EnkelTidslinje, Skalastørrelse, VedtaksperiodeStatus };
 export default Tidslinje;
--- packages/tidslinje/webpack.config.js
+++ packages/tidslinje/webpack.config.js
@@ -27,5 +27,6 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        library: 'helse-frontend-tidslinje'
     }
 };
