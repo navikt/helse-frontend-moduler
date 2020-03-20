989d2e0579077397c0ebe221491b2ad2b621042d
Stephen Ramthun <stephenramthun@gmail.com>
2020-03-20 13:22:03 +0100

    Eksporterer Dagstatus

--- packages/tabell/index.ts
+++ packages/tabell/index.ts
@@ -1,7 +1,7 @@
 import Tabell, { Body, Header, Footer } from './Tabell';
 import { Sykmeldingstabell, Utbetalingstabell } from './periodetabell';
-import { Dag, Dagtype, Kilde } from './periodetabell/types';
+import { Dag, Dagtype, Dagstatus, Kilde } from './periodetabell/types';
 
-export { Sykmeldingstabell, Utbetalingstabell, Tabell, Dag, Dagtype, Kilde, Body, Header, Footer };
+export { Sykmeldingstabell, Utbetalingstabell, Tabell, Dag, Dagtype, Dagstatus, Kilde, Body, Header, Footer };
 
 export default Tabell;
