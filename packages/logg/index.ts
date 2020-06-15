62d08a8f53aad5b568c1aa5cf28302b323852b08
Martin Solheim <martin.solheim@me.com>
2020-06-15 12:27:02 +0200

    Eksponerer Hendelsestatus
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- packages/logg/index.ts
+++ packages/logg/index.ts
@@ -1,8 +1,8 @@
 import Logg from './Logg';
-import { Hendelsetype, Hendelse } from './types';
+import { Hendelsetype, Hendelse, Hendelsestatus } from './types';
 import LoggProvider from './LoggContext';
 import Loggvisning from './components/Loggvisning';
 import LoggHeader from './components/LoggHeader';
 
 export default Logg;
-export { Hendelsetype, Hendelse, LoggProvider, Loggvisning, LoggHeader };
+export { Hendelsetype, Hendelsestatus, Hendelse, LoggProvider, Loggvisning, LoggHeader };
