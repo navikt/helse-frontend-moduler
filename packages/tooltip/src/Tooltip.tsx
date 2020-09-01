272b48dd2a148b51a0c2354fe2bf6d2ab4b19e24
Martin Solheim <martin.solheim@me.com>
2020-09-01 10:43:08 +0200

    Tar imot className på Tooltip
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>
    Co-authored-by: Erlend Viddal <eviddal@gmail.com>

--- packages/tooltip/src/Tooltip.tsx
+++ packages/tooltip/src/Tooltip.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
 import styles from './Tooltip.less';
+import classNames from 'classnames';
 
 interface TooltipProps {
     children: ReactNode | ReactNode[];
+    className?: string;
 }
 
-const Tooltip = ({ children }: TooltipProps) => {
-    return <div className={styles.tooltip}>{children}</div>;
+const Tooltip = ({ children, className }: TooltipProps) => {
+    return <div className={classNames(styles.tooltip, className)}>{children}</div>;
 };
 
 export default Tooltip;
