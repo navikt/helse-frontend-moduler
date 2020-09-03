46b01eb73c72594801e9a7eca9ffd0538a7b5a89
Stephen Ramthun <stephenramthun@gmail.com>
2020-09-03 15:14:08 +0200

    Fikser visning av tooltips

--- stories/tabeller/exampleData.tsx
+++ stories/tabeller/exampleData.tsx
@@ -19,29 +19,29 @@ export const rader = [
     ['19.02.2019', '100%', '1234 kr'],
     ['20.02.2019', '50%', '617 kr'],
     ['21.02.2019', '50%', '617 kr'],
-    ['22.02.2019', '80%', '987 kr']
+    ['22.02.2019', '80%', '987 kr'],
 ];
 
 export const sorterbareHeadere: (ReactNode | TabellHeader)[] = [
     {
         render: 'Dato',
-        sortFunction: (a: string, b: string) => tilMillisekunder(a) - tilMillisekunder(b)
+        sortFunction: (a: string, b: string) => tilMillisekunder(a) - tilMillisekunder(b),
     },
     {
         render: 'Grad',
-        sortFunction: (a: number, b: number) => a - b
+        sortFunction: (a: number, b: number) => a - b,
     },
     {
         render: 'Utbetaling',
-        sortFunction: (a: number, b: number) => a - b
-    }
+        sortFunction: (a: number, b: number) => a - b,
+    },
 ];
 
 export const sorterbareRader = [
     ['19.02.2019', 100, 1234],
     ['20.02.2019', 50, 617],
     ['21.02.2019', 50, 617],
-    ['22.02.2019', 80, 987]
+    ['22.02.2019', 80, 987],
 ];
 
 const renderer = (rad: ReactNode[]) => [rad[0], `${rad[1]}%`, `${rad[2]} kr`];
@@ -50,11 +50,11 @@ export const SorterbarTabell = () => {
     const { rader: tabellrader, headere: tabellheadere, sortering } = useTabell({
         rader: sorterbareRader,
         headere: sorterbareHeadere,
-        renderer
+        renderer,
     });
 
     return (
-        <Tabell beskrivelse="En sorterbar tabell" rader={tabellrader} headere={tabellheadere} sortering={sortering} />
+        <Tabell beskrivelse="En sorterbar tabell" rader={tabellrader} headere={tabellheadere} sortering={sortering}/>
     );
 };
 
@@ -64,10 +64,10 @@ const filtrerbareHeadere = [
         render: 'Grad',
         filtere: [
             { label: '< 100%', func: (grad: number) => grad < 100 },
-            { label: '< 75%', func: (grad: number) => grad < 75 }
-        ]
+            { label: '< 75%', func: (grad: number) => grad < 75 },
+        ],
     },
-    sorterbareHeadere[2]
+    sorterbareHeadere[2],
 ];
 
 const filtrerbareRader = [...sorterbareRader];
@@ -77,7 +77,7 @@ export const FiltrerbarTabell = () => {
     const { rader: tabellrader, headere: tabellheadere, ...rest } = useTabell({
         rader: sorterbareRader,
         headere: filtrerbareHeadere,
-        renderer
+        renderer,
     });
 
     console.log(rest.sortering);
@@ -93,9 +93,9 @@ export const HeaderOverFlereKolonnerTabell = () => {
             headere={[
                 {
                     render: 'Dato og gradering',
-                    kolonner: 2
+                    kolonner: 2,
                 },
-                'Utbetaling'
+                'Utbetaling',
             ]}
         />
     );
@@ -138,8 +138,8 @@ const Sideknapp = styled.button<{ active?: boolean }>`
     }
 
     ${({ active }) =>
-        active &&
-        `
+    active &&
+    `
         background: #0067c5;
         color: #fff;
         `}
@@ -155,22 +155,41 @@ export const pagineringsrader = [
     ['25.02.2019', 100, 1234],
     ['26.02.2019', 80, 987],
     ['27.02.2019', 100, 1234],
-    ['28.02.2019', 80, 987]
+    ['28.02.2019', 80, 987],
+    ['01.03.2019', 100, 1234],
+    ['02.03.2019', 100, 1234],
+    ['03.03.2019', 100, 1234],
+    ['04.03.2019', 100, 1234],
+    ['05.03.2019', 100, 1234],
+    ['06.03.2019', 100, 1234],
+    ['07.03.2019', 100, 1234],
+    ['08.03.2019', 100, 1234],
+    ['09.03.2019', 100, 1234],
+    ['10.03.2019', 80, 987],
+    ['11.03.2019', 80, 987],
+    ['12.03.2019', 80, 987],
+    ['13.03.2019', 80, 987],
+    ['14.03.2019', 100, 1234],
+    ['15.03.2019', 100, 1234],
+    ['16.03.2019', 100, 1234],
+    ['17.03.2019', 100, 1234],
+    ['18.03.2019', 100, 1234],
+    ['19.03.2019', 100, 1234],
 ];
 
 const _paginering: Paginering = {
     sidenummer: 1,
-    antallRaderPerSide: 4
+    antallRaderPerSide: 6,
 };
 
 export const PaginertTabell = () => {
     const renderer = (rad: ReactNode[]) => [rad[0], `${rad[1]}%`, `${rad[2]} kr`];
 
     const { rader: tabellrader, headere: tabellheadere, paginering, ...rest } = useTabell({
-        rader: [],
+        rader: pagineringsrader,
         headere: filtrerbareHeadere,
         renderer,
-        defaultPaginering: _paginering
+        defaultPaginering: _paginering,
     });
 
     const setSidenummer = (i: number) => paginering?.set(p => ({ ...p, sidenummer: i + 1 }));
@@ -178,21 +197,28 @@ export const PaginertTabell = () => {
     const incrementSidenummer = () =>
         paginering?.set(p => ({
             ...p,
-            sidenummer: p.sidenummer + 1 > paginering?.antallSider ? paginering.antallSider : p.sidenummer + 1
+            sidenummer: p.sidenummer + 1 > paginering?.antallSider ? paginering.antallSider : p.sidenummer + 1,
         }));
 
     const decrementSidenummer = () =>
         paginering?.set(p => ({
             ...p,
-            sidenummer: p.sidenummer - 1 < 1 ? 1 : p.sidenummer - 1
+            sidenummer: p.sidenummer - 1 < 1 ? 1 : p.sidenummer - 1,
         }));
 
     return (
         <>
-            <Tabell beskrivelse="en paginert tabell" rader={tabellrader} headere={tabellheadere} {...rest} />
+            <Tabell
+                beskrivelse="en paginert tabell"
+                rader={tabellrader}
+                headere={tabellheadere}
+                paginering={paginering}
+                {...rest}
+            />
             <BottomContainer>
                 <p>
-                    Viser {paginering?.førsteSynligeElement} til {paginering?.sisteSynligeElement} av {tabellrader.length} oppgaver
+                    Viser {paginering?.førsteSynligeElement} til {paginering?.sisteSynligeElement} av{' '}
+                    {tabellrader.length} oppgaver
                 </p>
                 {paginering?.antallSider! > 0 && (
                     <SideknappContainer>
