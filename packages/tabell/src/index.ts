19b82ba877e25168b7b2bc6c7e4366cf6d92ce1f
Stephen Ramthun <stephenramthun@gmail.com>
2020-10-01 08:43:40 +0200

    Tilgjengeliggjør lib fra index

--- packages/tabell/src/index.ts
+++ packages/tabell/src/index.ts
@@ -1,4 +1,14 @@
-import { Tabell } from './Tabell';
-import { useTabell } from './useTabell';
+import { Tabell, Tabellrader, Tabellheadere, TabellProps, Tabellrad } from './Tabell';
+import { useTabell, UseTabellPaginering, UseTabell, UseTabellOptions } from './useTabell';
 
-export { Tabell, useTabell };
+export {
+    Tabell,
+    Tabellrader,
+    Tabellheadere,
+    TabellProps,
+    Tabellrad,
+    useTabell,
+    UseTabellPaginering,
+    UseTabell,
+    UseTabellOptions
+};
